6441ca4755d536b21d819b24326106e55402da5c
StarFreedomX <starfreedomx@outlook.com>
2025-06-11 00:21:21 +0800

    fix: 改进修复数据库逻辑

--- src/index.ts
+++ src/index.ts
@@ -123,7 +123,7 @@ export function apply(ctx: Context, config: Config) {
       const localResult = await session.observeUser(['tsugu'])
       localResult.tsugu.platform = session.platform
       localResult.tsugu.userId = session.userId
-      localResult.tsugu.displayedServerList = [3, 0]
+      localResult.tsugu.displayedServerList = globalDefaultServer
       //修复数据库中的displayedServerList为string数组的问题
       localResult.tsugu.displayedServerList = stringArrayToNumberArray(localResult.tsugu.displayedServerList)
       // 将数组中的所有NaN替换为0，进行数据库的修复，防止发送NaN到后端导致反复出现后端报错
