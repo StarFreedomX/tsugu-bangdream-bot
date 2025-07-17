f8ada4655cd5cae1da9031af05319883ed1dd009
StarFreedomX <starfreedomx@outlook.com>
2025-07-17 09:18:11 +0800

    perf: 查岗支持指定活动

--- src/commands/topRateDetail.ts
+++ src/commands/topRateDetail.ts
@@ -2,9 +2,10 @@ import { Server } from "../types/Server";
 import { getReplyFromBackend } from "../api/getReplyFromBackend"
 import { Config } from '../config';
 
-export async function commandTopRateDetail(config: Config, count: number, playerId: string, tier: number, mainServer: Server): Promise<Array<Buffer | string>> {
+export async function commandTopRateDetail(config: Config, eventId: number, count: number, playerId: string, tier: number, mainServer: Server): Promise<Array<Buffer | string>> {
     return await getReplyFromBackend(`${config.backendUrl}/topRateDetail`, {
         mainServer,
+        eventId,
         count,
         playerId,
         tier,
